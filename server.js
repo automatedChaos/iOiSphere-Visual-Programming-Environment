3f7d9a890ca4d01fedc1b26fbe734381ca7ff57f
Alcwyn <alcwynparker@hotmail.co.uk>
2018-06-01 22:44:42 +0100

    changed the port

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var express = require('express')
 var app = express()
 
 // respond with "hello world" when a GET request is made to the homepage
-
+const PORT = process.env.PORT || 5000
 
 app.use(express.static('root'))
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(PORT, () => console.log('Example app listening on port 3000!'))
